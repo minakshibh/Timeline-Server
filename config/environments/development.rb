832608241aa09c09312916b2ae81720e96f5fda5
Babar Khan <babar.insonix@gmail.com>
2016-02-12 17:30:56 +0530

    change bucket name

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,7 +43,7 @@ Rails.application.configure do
   config.paperclip_defaults = {
       :storage => :s3,
       :s3_credentials => {
-          :bucket => "timeline-development-test",
+          :bucket => "timeline-new-development",
           :access_key_id => "AKIAIKBE6LY7XH4WDEHQ",
           :secret_access_key => "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"
       },

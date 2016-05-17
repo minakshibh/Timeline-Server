bee4671835e369a09a82f820c69a76b17ac8e6f1
Babar Khan <babar.insonix@gmail.com>
2016-05-17 12:49:54 +0530

    redis server setup

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,6 +1,5 @@
 class Activity < ActiveRecord::Base
   include ActiveUUID::UUID
-  include Backburner::Performable
 
   belongs_to :user
   belongs_to :trackable, :polymorphic => true
@@ -8,101 +7,16 @@ class Activity < ActiveRecord::Base
   after_save :send_notification
 
   def send_notification
-    Activity.delay.send_update
-  end
-
-  # private
-
-  def self.send_update
-
-    notification = nil
-
     if self.trackable.present?
+      puts "====self=#{self.id}=====trackable=#{self.trackable}"
       if self.trackable_type == 'Timeline'
-
-        external_id = self.trackable.user.external_id
-        user_id = self.trackable.user.id
-        user_query = {'__type' => 'Pointer', 'className' => '_User', 'objectId' => external_id}
-
-        if self.action == 'like'
-          notification = "@#{self.user.name} likes your feedeo ##{self.trackable.name}."
-          payload = {:timeline_id => self.trackable_id, :name => self.trackable.name, :action => self.action}
-        elsif self.action == 'follow'
-          notification = "@#{self.user.name} is now following your feedeo ##{self.trackable.name}."
-          payload = {:timeline_id => self.trackable_id, :name => self.trackable.name, :action => self.action}
-        elsif self.action == 'follow_request'
-          notification = "@#{self.user.name} wants to follow your feedeo ##{self.trackable.name}."
-          payload = {:timeline_id => self.trackable_id, :name => self.trackable.name, :action => self.action}
-        elsif self.action == 'create'
-          followers = User.select(:id, :external_id).where(id: Follow.select(:follower_id).where(followable_type: "User", follower_type: "User", followable_id: self.user.id))
-          external_id = {'$in' => followers.map { |u| u.external_id }.to_a.flatten}
-          notification = "@#{self.user.name} created feedeo ##{self.trackable.name}."
-          payload = {:timeline_id => self.trackable_id, :name => self.trackable.name, :action => self.action}
-          user_query = {'$inQuery' => {'where' => {'objectId' => external_id}, 'className' => '_User'}}
-        end
-
+        TimelineNotificationsWorker.perform_async(self.id)
       elsif self.trackable_type == 'User'
-        external_id = self.trackable.external_id
-        user_id = self.trackable.id
-        user_query = {'__type' => 'Pointer', 'className' => '_User', 'objectId' => external_id}
-        if self.action == 'like'
-          notification = "@#{self.user.name} likes your profile."
-          payload = {:user_id => self.user.id, :name => self.user.name, :external => self.user.external_id, :action => self.action}
-        elsif self.action == 'follow'
-          notification = "@#{self.user.name} is now following you."
-          payload = {:user_id => self.user.id, :name => self.user.name, :external => self.user.external_id, :action => self.action}
-        elsif self.action == 'follow_request'
-          notification = "@#{self.user.name} wants to follow you."
-          payload = {:user_id => self.user.id, :name => self.user.name, :external => self.user.external_id, :action => self.action}
-        elsif self.action == 'follow_accept'
-          notification = "@#{self.user.name} accepted your following request."
-          payload = {:user_id => self.user.id, :name => self.user.name, :external => self.user.external_id, :action => self.action}
-        end
+        UserNotificationsWorker.perform_async(self.id)
       elsif self.trackable_type == 'Video'
-        followers = User.select(:id, :external_id).where(id: Follow.select(:follower_id).where(followable_type: "Timeline", follower_type: "User", followable_id: self.trackable.timeline.id))
-        external_id = {'$in' => followers.map { |u| u.external_id }.to_a.flatten}
-        notification = "@#{self.user.name} added a moment to ##{self.trackable.timeline.name}."
-        payload = {:timeline_id => self.trackable.timeline.id, :video_id => self.trackable_id, :name => self.trackable.timeline.name, :action => self.action}
-        user_query = {'$inQuery' => {'where' => {'objectId' => external_id}, 'className' => '_User'}}
-      end
-
-      pushdata = {:where => {:user => user_query},
-                  :data => {:alert => notification, :badge => 'Increment', :sound => 'default', 'content-available' => 1, :payload => payload}}
-
-      # send REST call to Parse if we have a notification
-      if notification
-        uri = URI.parse("https://api.parse.com/1/push")
-
-        http = Net::HTTP.new(uri.host, uri.port)
-        http.use_ssl = true if uri.scheme == 'https'
-
-        response = nil
-
-        http.start do |h|
-          request = Net::HTTP::Post.new uri.request_uri
-          request['X-Parse-Application-Id'] = PARSE_CONFIG['PARSE_APP_ID']
-          request['X-Parse-REST-API-Key'] = PARSE_CONFIG['PARSE_API_KEY']
-          request['Content-Type'] = "application/json"
-
-          request.body = pushdata.to_json
-          # puts request.body
-          response = h.request request
-          # puts response
-        end
-        if !followers
-          Notification.create(:user_id => user_id, :reportable => self.trackable, :notification => notification, :payload => payload.to_json)
-        else
-          followers.each do |f|
-            Notification.create(:user_id => f.id, :reportable => self.trackable, :notification => notification, :payload => payload.to_json)
-          end
-        end
+        VideoNotificationWorker.perform_async(self.id)
       end
     end
-
   end
-
-
-
-  # handle_asynchronously :send_notification, queue: 'notifications'
 
 end
---
+++ app/workers/video_notifications_worker.rb
@@ -0,0 +1,45 @@
+class VideoNotificationWorker
+  include Sidekiq::Worker
+  sidekiq_options queue: 'high'
+
+  def perform(activity_id)
+    activity = Activity.find_by_id(activity_id)
+    followers = User.select(:id, :external_id).where(id: Follow.select(:follower_id).where(followable_type: "Timeline", follower_type: "User", followable_id: activity.trackable.timeline.id))
+    external_id = {'$in' => followers.map { |u| u.external_id }.to_a.flatten}
+    notification = "@#{activity.user.name} added a moment to ##{activity.trackable.timeline.name}."
+    payload = {:timeline_id => activity.trackable.timeline.id, :video_id => activity.trackable_id, :name => activity.trackable.timeline.name, :action => activity.action}
+    user_query = {'$inQuery' => {'where' => {'objectId' => external_id}, 'className' => '_User'}}
+
+
+    pushdata = {:where => {:user => user_query},
+                :data => {:alert => notification, :badge => 'Increment', :sound => 'default', 'content-available' => 1, :payload => payload}}
+
+    # send REST call to Parse if we have a notification
+    uri = URI.parse("https://api.parse.com/1/push")
+
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true if uri.scheme == 'https'
+
+    response = nil
+
+    http.start do |h|
+      request = Net::HTTP::Post.new uri.request_uri
+      request['X-Parse-Application-Id'] = PARSE_CONFIG['PARSE_APP_ID']
+      request['X-Parse-REST-API-Key'] = PARSE_CONFIG['PARSE_API_KEY']
+      request['Content-Type'] = "application/json"
+
+      request.body = pushdata.to_json
+      # puts request.body
+      response = h.request request
+      # puts response
+    end
+    if !followers
+      Notification.create(:user_id => user_id, :reportable => activity.trackable, :notification => notification, :payload => payload.to_json)
+    else
+      followers.each do |f|
+        Notification.create(:user_id => f.id, :reportable => activity.trackable, :notification => notification, :payload => payload.to_json)
+      end
+    end
+  end
+
+end

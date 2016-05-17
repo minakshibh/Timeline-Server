bd665405d4c7c84f51a65645fe1f4f48d61503f2
Babar Khan <babar.insonix@gmail.com>
2016-05-17 13:41:59 +0530

    adding reportable_id and repotable_type in group_alert

--- app/workers/tagging_alert_worker.rb
+++ app/workers/tagging_alert_worker.rb
@@ -1,41 +1,47 @@
 class TaggingAlertWorker
   include Sidekiq::Worker
   sidekiq_options queue: 'high'
-  # sidekiq_options retry: false
+  sidekiq_options retry: false
+  sidekiq_options unique: :until_and_while_executing
 
-  def perform(notification,payload,users)
+
+  def perform(notification, payload, users)
     reportable_type = users.pop()
     reportable_id = users.pop()
-    users = User.where(:id=>users)
+    users = User.where(:id => users)
     external_id = {'$in' => users.map { |u| u.external_id }.to_a.flatten}
     user_query = {'$inQuery' => {'where' => {'objectId' => external_id}, 'className' => '_User'}}
     push_data = {:where => {:user => user_query},
                  :data => {:alert => notification, :badge => 'Increment', :sound => 'default', 'content-available' => 1, :payload => payload}}
     #sending push to parse server
-    uri = URI.parse("https://api.parse.com/1/push")
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true if uri.scheme == 'https'
-    response = nil
 
-    http.start do |h|
-      request = Net::HTTP::Post.new uri.request_uri
-      request['X-Parse-Application-Id'] = PARSE_CONFIG['PARSE_APP_ID']
-      request['X-Parse-REST-API-Key'] = PARSE_CONFIG['PARSE_API_KEY']
-      request['Content-Type'] = "application/json"
+    if notification
+      uri = URI.parse("https://api.parse.com/1/push")
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true if uri.scheme == 'https'
+      response = nil
 
-      request.body = push_data.to_json
-      # puts request.body
-      response = h.request request
-      # puts response
+      http.start do |h|
+        request = Net::HTTP::Post.new uri.request_uri
+        request['X-Parse-Application-Id'] = PARSE_CONFIG['PARSE_APP_ID']
+        request['X-Parse-REST-API-Key'] = PARSE_CONFIG['PARSE_API_KEY']
+        request['Content-Type'] = "application/json"
+
+        request.body = push_data.to_json
+        # puts request.body
+        response = h.request request
+        # puts response
+      end
+
+      case reportable_type
+        when 'Timeline'
+          reportable = Timeline.find_by_id(reportable_id)
+        when 'video'
+          reportable = Video.find_by_id(reportable_id)
+      end
+      # create user notification into db
+      users.each { |user| Notification.create(:user_id => user.id, :reportable => reportable, :notification => notification, :payload => payload.merge!(:user_id => user.id).to_json) }
     end
 
-    case reportable_type
-      when 'Timeline'
-        reportable = Timeline.find_by_id(reportable_id)
-      when 'video'
-        reportable = Video.find_by_id(reportable_id)
-    end
-    # create user notification into db
-    users.each { |user| Notification.create(:user_id => user.id, :reportable => reportable, :notification => notification, :payload => payload.merge!(:user_id => user.id).to_json) }
   end
 end
--- app/workers/user_notifications_worker.rb
+++ app/workers/user_notifications_worker.rb
@@ -1,9 +1,12 @@
 class UserNotificationsWorker
   include Sidekiq::Worker
   sidekiq_options queue: 'high'
+  sidekiq_options retry: false
+  sidekiq_options unique: :until_and_while_executing
 
   def perform(activity_id)
     activity = Activity.find_by_id(activity_id)
+    notification = nil
     followers = nil
     external_id = activity.trackable.external_id
     user_id = activity.trackable.id
@@ -26,29 +29,31 @@ class UserNotificationsWorker
                 :data => {:alert => notification, :badge => 'Increment', :sound => 'default', 'content-available' => 1, :payload => payload}}
 
     # send REST call to Parse if we have a notification
-    uri = URI.parse("https://api.parse.com/1/push")
+    if notification
+      uri = URI.parse("https://api.parse.com/1/push")
 
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true if uri.scheme == 'https'
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true if uri.scheme == 'https'
 
-    response = nil
+      response = nil
 
-    http.start do |h|
-      request = Net::HTTP::Post.new uri.request_uri
-      request['X-Parse-Application-Id'] = PARSE_CONFIG['PARSE_APP_ID']
-      request['X-Parse-REST-API-Key'] = PARSE_CONFIG['PARSE_API_KEY']
-      request['Content-Type'] = "application/json"
+      http.start do |h|
+        request = Net::HTTP::Post.new uri.request_uri
+        request['X-Parse-Application-Id'] = PARSE_CONFIG['PARSE_APP_ID']
+        request['X-Parse-REST-API-Key'] = PARSE_CONFIG['PARSE_API_KEY']
+        request['Content-Type'] = "application/json"
 
-      request.body = pushdata.to_json
-      # puts request.body
-      response = h.request request
-      # puts response
-    end
-    if !followers
-      Notification.create(:user_id => user_id, :reportable => activity.trackable, :notification => notification, :payload => payload.to_json)
-    else
-      followers.each do |f|
-        Notification.create(:user_id => f.id, :reportable => activity.trackable, :notification => notification, :payload => payload.to_json)
+        request.body = pushdata.to_json
+        # puts request.body
+        response = h.request request
+        # puts response
+      end
+      if !followers
+        Notification.create(:user_id => user_id, :reportable => activity.trackable, :notification => notification, :payload => payload.to_json)
+      else
+        followers.each do |f|
+          Notification.create(:user_id => f.id, :reportable => activity.trackable, :notification => notification, :payload => payload.to_json)
+        end
       end
     end
 

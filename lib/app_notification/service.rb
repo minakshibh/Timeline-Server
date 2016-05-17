ffda8d8b86db0a1c86600c1bce22c70bcf87f189
Babar Khan <babar.insonix@gmail.com>
2016-05-17 13:34:35 +0530

    adding reportable_id and repotable_type in group_alert

--- lib/app_notification/service.rb
+++ lib/app_notification/service.rb
@@ -1,15 +1,13 @@
 module AppNotification
   class Service
     def self.tagging_users_push(notification, users, payload, reportable_id,reportable_type)
-      users = users.map{|user| user.id}
-      users = users.push(reportable_id,reportable_type)
+      users = users.map{|user| user.id}.push(reportable_id,reportable_type)
       TaggingAlertWorker.perform_async(notification,payload,users)
-      # Backburner.enqueue Jobs::NotificationJob, notification, external_id, payload
     end
 
     def self.adding_moment_push(notification, users, payload, reportable_id,reportable_type)
-      users = users.map{|user| user.id}
-      TaggingAlertWorker.perform_async(notification,payload,users,reportable_id,reportable_type)
+      users = users.map{|user| user.id}.push(reportable_id,reportable_type)
+      TaggingAlertWorker.perform_async(notification,payload,users)
     end
   end
 end
